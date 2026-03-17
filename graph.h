cc4e74d74ae1d50d528950edf2693c74f4729dae
Matheus G. M. Arcanjo <mathgarcanjo@gmail.com>
2026-03-17 15:14:21 -0300

    feat: atualização do .h

--- graph.h
+++ graph.h
@@ -1,1 +1,15 @@
-/* comentario */
+#ifndef GRAPH_H
+    #define GRAPH_H
+    #include <stdbool.h>
+
+    typedef struct Graph GRAPH;
+    GRAPH* MyGraph(int v);
+    void add_edge(GRAPH* G,int v1, int v2, int peso); 
+    bool exist_edge(GRAPH* G, int v1, int v2);
+    int* neighbors(GRAPH* G, int v);
+    void remove_edge(GRAPH* G,int v1,int v2); 
+    void print_info(GRAPH* G);
+    void delete_graph(GRAPH** G); 
+    int max_neighbors(GRAPH* G);
+    int** adjacency_matrix(GRAPH *G);
+#endif
